08f9427dbb7dfdd7f7b578ae8eef0d4472f78753
Patryk Łubik <patryk.lubik@wp.pl>
2020-11-12 19:26:46 +0100

    improved balance view

---
+++ public/js/balance.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+    if(!$("#expenseList").children().length) {
+        $("#pieChartTitle").attr('hidden', 'hidden');
+        $("figure").attr('hidden', 'hidden');
+    }
+
+});
